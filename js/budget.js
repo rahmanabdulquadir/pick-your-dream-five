c661614364a9a7b715765463eb4b89c2420b01c6
rahmanabdulquadir <rahmanaq777@gmail.comm>
2022-08-23 23:21:37 +0600

    finished

--- js/budget.js
+++ js/budget.js
@@ -1,50 +1,45 @@
-
-
-document.getElementById('btn-calculate').addEventListener('click', function(){
-  const perPlayer = document.getElementById('price-field');
+document.getElementById("btn-calculate").addEventListener("click", function () {
+  const perPlayer = document.getElementById("price-field");
   const perPlayerValueString = perPlayer.value;
   const perPlayerValue = parseInt(perPlayerValueString);
-  
 
-  const playerExpense = document.getElementById('player-expense');
+  const playerExpense = document.getElementById("player-expense");
   const playerExpenseValueString = playerExpense.innerText;
   const playerExpenseValue = parseInt(playerExpenseValueString);
-  
 
   const totalExpense = perPlayerValue * list.length;
   playerExpense.innerText = totalExpense;
-  perPlayer.value = ''
+  perPlayer.value = "";
+});
 
-})
+document
+  .getElementById("btn-total-calculation")
+  .addEventListener("click", function () {
+    const perPlayer = document.getElementById("price-field");
+    const perPlayerValueString = perPlayer.value;
+    const perPlayerValue = parseInt(perPlayerValueString);
 
-document.getElementById('btn-total-calculation').addEventListener('click', function(){
-  const perPlayer = document.getElementById('price-field');
-  const perPlayerValueString = perPlayer.value;
-  const perPlayerValue = parseInt(perPlayerValueString);
-  
+    const playerExpense = document.getElementById("player-expense");
+    const playerExpenseValueString = playerExpense.innerText;
+    const playerExpenseValue = parseInt(playerExpenseValueString);
 
-  const playerExpense = document.getElementById('player-expense');
-  const playerExpenseValueString = playerExpense.innerText;
-  const playerExpenseValue = parseInt(playerExpenseValueString);
-  
+    const totalExpense = perPlayerValue * list.length;
+    playerExpense.innerText = totalExpense;
 
-  const totalExpense = perPlayerValue * list.length;
-  playerExpense.innerText = totalExpense;
+    const managerField = document.getElementById("manager-field");
+    const managerFieldValueString = managerField.value;
+    const managerFieldValue = parseInt(managerFieldValueString);
 
+    const coachField = document.getElementById("coach-field");
+    const coachFieldValueString = coachField.value;
+    const coachFieldValue = parseInt(coachFieldValueString);
 
-  const managerField = document.getElementById('manager-field');
-  const managerFieldValueString = managerField.value;
-  const managerFieldValue = parseInt(managerFieldValueString);
+    const totalExpenseAmount = document.getElementById("total-expense-amount");
+    const totalExpenseAmountValueString = totalExpenseAmount.innerText;
+    const totalExpenseAmountValue = parseInt(totalExpenseAmountValueString);
 
-  const coachField = document.getElementById('coach-field');
-  const coachFieldValueString = coachField.value;
-  const coachFieldValue = parseInt(coachFieldValueString);
-
-  const totalExpenseAmount = document.getElementById('total-expense-amount');
-  const totalExpenseAmountValueString = totalExpenseAmount.innerText;
-  const totalExpenseAmountValue = parseInt(totalExpenseAmountValueString);
-
-  const finalTotalExpenseAmount = managerFieldValue + coachFieldValue + playerExpenseValue;
-  totalExpenseAmount.innerText = finalTotalExpenseAmount;
-  console.log(totalExpenseAmount);
-})
+    const finalTotalExpenseAmount =
+      managerFieldValue + coachFieldValue + playerExpenseValue;
+    totalExpenseAmount.innerText = finalTotalExpenseAmount;
+    console.log(totalExpenseAmount);
+  });
--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
 const list = [];
 
-function playerList(){
-  const listContainer = document.getElementById('selected-five');
+function playerList() {
+  const listContainer = document.getElementById("selected-five");
 
-  if(list.length > 5){
-    alert('Max number of player already selected')
-  }
-  else{
-    listContainer.textContent = '';
-    for(let i = 0; i < list.length; i++){
-      const li = document.createElement('li');
-      li.innerHTML= `
+  if (list.length > 5) {
+    alert("Max number of player already selected");
+  } else {
+    listContainer.textContent = "";
+    for (let i = 0; i < list.length; i++) {
+      const li = document.createElement("li");
+      li.innerHTML = `
       <li>${i + 1} ${list[i].playerName}</li>
       `;
       listContainer.appendChild(li);
-      
     }
   }
- 
 }
 
-function addToList(element){
+function addToList(element) {
   const playerName = element.parentNode.parentNode.children[0].innerText;
   const pd = {
-    playerName : playerName,
-  }
-
+    playerName: playerName,
+  };
 
   list.push(pd);
 
   playerList();
 }
- console.log('new file added')
+console.log("new file added");
